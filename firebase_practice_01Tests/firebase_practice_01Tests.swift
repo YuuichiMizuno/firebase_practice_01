ad373fcd4c38862bcc59d4290adf3189d2fd59ee
yuichi.watanabe <yuichi.watanabe@booklive.co.jp>
2016-11-15 14:27:55 +0900

    Initial Commit

---
+++ firebase_practice_01Tests/firebase_practice_01Tests.swift
@@ -0,0 +1,36 @@
+//
+//  firebase_practice_01Tests.swift
+//  firebase_practice_01Tests
+//
+//  Created by yuichi.watanabe on 2016/11/15.
+//  Copyright © 2016年 yuichi.watanabe. All rights reserved.
+//
+
+import XCTest
+@testable import firebase_practice_01
+
+class firebase_practice_01Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
